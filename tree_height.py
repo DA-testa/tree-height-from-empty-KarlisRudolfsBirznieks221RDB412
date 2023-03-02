2aaec55ea8b7e52c70dd1cc670844356a353091a
Kārlis Rūdolfs Birznieks <125714170+KarlisRudolfsBirznieks221RDB412@users.noreply.github.com>
2023-03-02 13:55:58 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,44 @@
-# python3
-
 import sys
 import threading
-import numpy
+import numpy as np
 
+def calculate_height(n, parents):
+    height = [-1] * n
 
-def compute_height(n, parents):
-    # Write this function
+    def get_height(node):
+        if height[node] != -1:
+            return height[node]
+        if parents[node] == -1:
+            height[node] = 1
+        else:
+            height[node] = get_height(parents[node]) + 1
+        return height[node]
+
     max_height = 0
-    # Your code here
+    for i in range(n):
+        max_height = max(max_height, get_height(i))
+
     return max_height
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    input_str = input().strip()
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+    if "I" in input_str:
+        n = int(input())
+        parents = list(map(int, input().split()))
+        print(calculate_height(n, parents))
+
+    if "F" in input_str:
+        filename = input().strip()
+        filepath = "test/" + filename
+        with open(filepath, 'r') as file:
+            n = int(file.readline())
+            parents = list(map(int, file.readline().split()))
+        print(calculate_height(n, parents))
+
+    print(np.array([1, 2, 3]))
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
